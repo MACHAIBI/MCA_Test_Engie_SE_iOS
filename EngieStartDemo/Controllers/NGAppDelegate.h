91e1beac64d961c63a2381dcbc6a3c4c4495d83e
Abdelmadjid CHAIBI <majidnewz12@gmail.com>
2017-06-05 09:55:14 +0200

    [Master] Initial project

---
+++ EngieStartDemo/Controllers/NGAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  NGAppDelegate.h
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 6/5/17
+//  Copyright (c) 2017 Engie. All rights reserved.
+//
+
+@interface NGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EngieStartDemo/Resources/Other-Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 6/5/17
+//  Copyright (c) 2017 Engie. All rights reserved.
+//
+
+@import UIKit;
+
+#import "NGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NGAppDelegate class]));
+    }
+}
