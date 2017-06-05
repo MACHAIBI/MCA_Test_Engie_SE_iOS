a593c6cdef46037d7d553dd648c53aed3c62e1f2
Abdelmadjid CHAIBI <majidnewz12@gmail.com>
2017-06-05 13:55:58 +0200

    [Dev] add NGUser

---
+++ EngieStartDemo/Models/NGUser.h
@@ -0,0 +1,21 @@
+//
+//  NGUser.h
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 05/06/2017.
+//  Copyright © 2017 Engie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NGUser : NSObject
+
+@property (nonatomic, strong) NSString *idUser;
+@property (nonatomic, strong) NSString *email;
+@property (nonatomic, strong) NSString *name;
+
+#pragma mark - Class Methods
+
++ (id)sharedInstance;
+
+@end
---
+++ EngieStartDemo/Models/NGUser.m
@@ -0,0 +1,26 @@
+//
+//  NGUser.m
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 05/06/2017.
+//  Copyright © 2017 Engie. All rights reserved.
+//
+
+#import "NGUser.h"
+
+@implementation NGUser
+
+#pragma mark - Class Methods
+
+static NGUser *_sharedInstance = nil;
+
++ (instancetype)sharedInstance
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[self alloc] init];
+    });
+    return _sharedInstance;
+}
+
+@end
