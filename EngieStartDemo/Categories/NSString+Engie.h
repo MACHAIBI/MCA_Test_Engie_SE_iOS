246f8606ac85c3c6cb2eff8fd204a3ea4d2ea0ab
Abdelmadjid CHAIBI <majidnewz12@gmail.com>
2017-06-05 12:03:35 +0200

    [Dev] add NSString Category

---
+++ EngieStartDemo/Categories/NSString+Engie.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Engie.h
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 05/06/2017.
+//  Copyright © 2017 Engie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Engie)
+
+- (BOOL) isEmail;
+
+@end
---
+++ EngieStartDemo/Categories/NSString+Engie.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Engie.m
+//  EngieStartDemo
+//
+//  Created by CHAIBI  on 05/06/2017.
+//  Copyright © 2017 Engie. All rights reserved.
+//
+
+#import "NSString+Engie.h"
+
+@implementation NSString (Engie)
+
+@end
